d6220bf178fee26a7700ba99fc8cec56816aaf4d
Ivory <qiaofei98@gmail.com>
2024-07-28 14:00:10 +1200

    Added feature 2: return name with markdown formatting

--- Greeter.java
+++ Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name.toUpperCase();
+    return messages.getString("greeting") + " *" + name + "*";
 }
 
   public static void main(String[] args) {
